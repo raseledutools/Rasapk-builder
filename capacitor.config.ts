9bafa0d5d2d9bb3b176babe723058318a7bd0f2e
raseledutools <bhmrasel60@gmail.com>
2026-04-24 12:44:04 +0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,8 +5,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.capacitorjs.app.testapp',
-  appName: 'capacitor-testapp',
+  appId: 'com.capacitorjs.app.testapp', 
+  appName: 'RasFocus',
   webDir: 'dist',
   plugins: {
     CapacitorCookies: {
@@ -16,7 +16,12 @@ const config: CapacitorConfig = {
       enabled: true,
     },
     SplashScreen: {
-      launchAutoHide: false,
+      launchAutoHide: true,       // এটি এখন অটোমেটিক সরে যাবে
+      launchShowDuration: 3000,   // ৩ সেকেন্ড পর্যন্ত স্প্ল্যাশ দেখাবে
+      backgroundColor: "#020617", // আপনার ডার্ক থিমের সাথে মেলানো কালার
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      spinnerColor: "#ec4899"
     },
     LocalNotifications: {
       smallIcon: 'ic_stat_icon_config_sample',
