31217f131d2f5d45e1f4fd8d7a72dd3fb228d46d
raseledutools <bhmrasel60@gmail.com>
2026-04-24 15:07:56 +0600

    Update MainActivity.java

--- android/app/src/main/java/com/capacitorjs/testapp/MainActivity.java
+++ android/app/src/main/java/com/capacitorjs/testapp/MainActivity.java
@@ -1,5 +1,14 @@
 package com.capacitorjs.app.testapp;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity { }
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // আমাদের নতুন AppBlocker প্লাগিনটি এখানে কানেক্ট করে দিলাম
+        registerPlugin(AppBlockerPlugin.class);
+    }
+}
